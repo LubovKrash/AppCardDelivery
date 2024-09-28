95118a87165b4f89baf46d1b48bb2d0c6d15e893
Любовь <altukhovalv@yandex.ru>
2024-09-28 14:10:00 +0300

    data test

---
+++ src/test/java/ru/netology/web/AppCardDeliveryTest.java
@@ -0,0 +1,29 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class AppCardDeliveryTest {
+
+    private String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    public void letMeTest() {
+        Selenide.open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Иваново");
+        String enteredDate = generateDate(1, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(enteredDate);
+          }
+}
